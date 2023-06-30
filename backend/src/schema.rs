51807803c84ebb91a6e42b699fa88602c03bb73b
Espequair <espequair@gmail.com>
2023-06-30 22:24:53 +0200

    [Diesel] schema

---
+++ backend/src/schema.rs
@@ -0,0 +1,44 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    ingredient (id) {
+        id -> Int4,
+        unit -> Nullable<Varchar>,
+        label -> Nullable<Varchar>,
+    }
+}
+
+diesel::table! {
+    instructions (id) {
+        id -> Int4,
+        instruction -> Nullable<Varchar>,
+        display_order -> Nullable<Int4>,
+        recipe_id -> Nullable<Int4>,
+    }
+}
+
+diesel::table! {
+    recipe_ingredients (id) {
+        id -> Int4,
+        amount -> Nullable<Float8>,
+        recipe_id -> Nullable<Int4>,
+        ingredient_id -> Nullable<Int4>,
+    }
+}
+
+diesel::table! {
+    recipes (id) {
+        id -> Int4,
+        title -> Varchar,
+        servings -> Varchar,
+        created_at -> Nullable<Timestamp>,
+        updated_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    ingredient,
+    instructions,
+    recipe_ingredients,
+    recipes,
+);

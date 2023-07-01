e75cd660a1fff0503bea124545a0eafdd237661e
Caio Mauro <caiomauro18@gmail.com>
2023-07-01 13:09:14 -0400

    Request to backend for data
    fetch to /recipes, displays data on web page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,37 @@
 import Image from "next/image";
+"use client";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await fetch("/recipes");
+        if (response.ok) {
+          const data = await response.json();
+          setRecipes(data.results);
+        } else {
+          throw new Error("request failed");
+        }
+      } catch (error) {
+        console.error(error);
+      } 
+    };
+
+    fetchRecipes();
+  }, []);
+
   return (
     <main>
       <h1 className="text-3xl font-extrabold">Recipe App</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}> {recipe.title} </li>
+        ))}
+      </ul>
     </main>
   );
 }

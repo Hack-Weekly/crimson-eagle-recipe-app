e218c4f0b9937b6cfcc68911ceaeac0336c1762d
Caio Mauro <caiomauro18@gmail.com>
2023-07-02 15:01:44 -0400

    Update page.tsx (cards, form, filter box)
    Data is displayed, data is able to be added.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,13 +2,81 @@
 import SearchBar from "@/components/SearchBar";
 import { Icon } from "@iconify/react";
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
+
+interface Recipe {
+  id: number;
+  title: string;
+  servings: string;
+  created_at: string | null;
+  updated_at: string | null;
+}
+
 export default function Home() {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [showForm, setShowForm] = useState(false);
+  const [recipeTitle, setRecipeTitle] = useState("");
+  const [recipeId, setRecipeId] = useState("");
+  const [recipeServings, setRecipeServings] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://crimson-eagles-recipe-app.onrender.com/recipes");
+        const data = await response.json();
+        setRecipes(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleAddRecipeClick = () => {
+    setShowForm(true);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  
+    const recipeData = {
+      title: recipeTitle,
+      id: recipeId,
+      servings: recipeServings,
+    };
+  
+    try {
+      const response = await fetch("https://crimson-eagles-recipe-app.onrender.com/addrecipes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(recipeData),
+      });
+  
+      if (response.ok) {
+        const newRecipe = await response.json();
+        setRecipes((prevRecipes) => [...prevRecipes, newRecipe]);
+      } else {
+        console.error("Failed to add recipe:", response.status);
+      }
+    } catch (error) {
+      console.error("Error occurred while adding recipe:", error);
+    }
+  
+    setRecipeTitle("");
+    setRecipeId("");
+    setRecipeServings("");
+    setShowForm(false);
+  }; 
+
   return (
     <main className="h-full w-full flex  flex-col content-center justify-center py-10">
       <h1 className="text-6xl font-extrabold text-center mb-10">Foodly</h1>
-      <div className="flex justify-end items-center gap-12 pr-10">
+      <div className="flex justify-end items-center gap-12 pr-10 mb-8">
         <SearchBar />
-        <button className="flex justify-between items-center px-4 py-5 h-6 w-40 bg-red-500 rounded-2xl text-white">
+        <button className="flex justify-between items-center px-4 py-5 h-6 w-40 bg-red-500 rounded-2xl text-white" onClick={handleAddRecipeClick}>
           <Icon icon="basil:add-solid" className="w-8 h-8" />
           <span className="text-lg font-bold"> Add recipe</span>
         </button>
@@ -17,6 +85,86 @@ export default function Home() {
           <span className="text-lg font-bold"> Bookmarks</span>
         </button>
       </div>
+
+    <div className="flex justify-center gap-8">
+      <div className="w-1/4 bg-red-500 text-white flex flex-col items-center justify-start rounded-lg p-6 h-screen">
+          <h2 className="text-2xl font-bold mt-4">Filters</h2>
+      </div>
+      <div className="w-3/4">
+        <div className="flex flex-wrap justify-start gap-8">
+          {recipes.map((recipe) => (
+            <div
+              key={recipe.id}
+              className="bg-white rounded-lg shadow-lg p-4 sm:p-6 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-black mb-8"
+            >
+              <h2 className="text-2xl font-bold mb-2">{recipe.title}</h2>
+              <p className="text-lg mb-2">Servings: {recipe.servings}</p>
+              <p className="text-sm mb-2">
+                Created At: {recipe.created_at || "N/A"}
+              </p>
+              <p className="text-sm">
+                Updated At: {recipe.updated_at || "N/A"}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+
+    {showForm && (
+        <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-200 bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white rounded-lg shadow-lg p-6">
+            <h2 className="text-2xl font-bold mb-4">Add Recipe</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label htmlFor="title" className="block font-bold mb-2">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  id="title"
+                  value={recipeTitle}
+                  onChange={(e) => setRecipeTitle(e.target.value)}
+                  className="border border-gray-300 px-3 py-2 rounded-md w-full"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="id" className="block font-bold mb-2">
+                  ID
+                </label>
+                <input
+                  type="text"
+                  id="id"
+                  value={recipeId}
+                  onChange={(e) => setRecipeId(e.target.value)}
+                  className="border border-gray-300 px-3 py-2 rounded-md w-full"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="title" className="block font-bold mb-2">
+                  Servings
+                </label>
+                <input
+                  type="text"
+                  id="servings"
+                  value={recipeServings}
+                  onChange={(e) => setRecipeServings(e.target.value)}
+                  className="border border-gray-300 px-3 py-2 rounded-md w-full"
+                  required
+                />
+              </div>
+              <button type="submit"className="bg-red-500 text-white px-4 py-2 rounded-md">
+                Add
+              </button>
+            </form>
+            <button className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md mt-4" onClick={() => setShowForm(false)}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
     </main>
   );
 }

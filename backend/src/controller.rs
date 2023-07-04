992b40fc22fb32984326a6ad89a56dbe3298102e
FrolickingMustelid <kszolgyen@gmail.com>
2023-07-04 20:46:44 +0200

    Instructions relationship (/recipes/[id] only)

--- backend/src/controller.rs
+++ backend/src/controller.rs
@@ -5,12 +5,13 @@ use rocket::serde::json::Json;
 use crate::database;
 use crate::models::*;
 use crate::schema::recipes::dsl::*;
+use crate::schema::*;
 
 #[get("/recipes")]
-pub fn recipe() -> Json<Vec<Recipes>> {
+pub fn recipe() -> Json<Vec<Recipe>> {
     let connection = &mut database::establish_connection();
     recipes
-        .load::<Recipes>(connection)
+        .load::<Recipe>(connection)
         .map(Json)
         .expect("Error loading recipes")
 }
@@ -20,7 +21,7 @@ pub fn search(
     query: String,
     page: Option<i64>,
     per_page: Option<i64>,
-) -> Result<Json<Vec<Recipes>>, Status> {
+) -> Result<Json<Vec<Recipe>>, Status> {
     let connection = &mut database::establish_connection();
 
     let page_number = page.unwrap_or(1);
@@ -33,7 +34,7 @@ pub fn search(
         .order(title.asc())
         .limit(recipes_per_page)
         .offset(offset)
-        .load::<Recipes>(connection);
+        .load::<Recipe>(connection);
 
     match results {
         Ok(results) => Ok(Json(results)),
@@ -42,32 +43,50 @@ pub fn search(
 }
 
 #[get("/recipes/<recipe_id>")]
-pub fn single_recipe(recipe_id: i32) -> Result<Json<Recipes>, Status> {
+pub fn single_recipe(recipe_id: i32) -> Result<Json<RecipeResultDTO>, Status> {
     let connection = &mut database::establish_connection();
 
-    match recipes.find(recipe_id).first::<Recipes>(connection) {
-        Ok(res) => Ok(rocket::serde::json::Json(res)),
+    let inst = match instructions::table
+        .filter(instructions::recipe_id.eq(recipe_id))
+        .order(instructions::display_order.asc())
+        .load::<Instruction>(connection)
+    {
+        Ok(res) => res,
+        Err(_) => return Err(Status::InternalServerError),
+    };
+
+    match recipes.find(recipe_id).first::<Recipe>(connection) {
+        Ok(res) => {
+            let mut recipe_with_inst = RecipeResultDTO::from(res);
+            recipe_with_inst.instructions = inst
+                .iter()
+                .map(|i| i.instruction.clone())
+                .collect::<Vec<String>>();
+            Ok(Json(recipe_with_inst))
+        }
         Err(_) => Err(Status::NotFound),
     }
 }
 
 #[post("/recipes", data = "<addrecipes>")]
-pub fn addrecipes(addrecipes: Json<RecipesInput>) -> Result<Json<Recipes>, Status> {
+pub fn addrecipes(addrecipes: Json<RecipesInput>) -> Result<Json<Recipe>, Status> {
     use crate::schema::recipes;
 
     let connection = &mut database::establish_connection();
     match diesel::insert_into(recipes::table)
         .values(addrecipes.into_inner())
-        .execute(connection) {
-            Ok(_) => (),
-            Err(_) => return Err(Status::InternalServerError),
+        .execute(connection)
+    {
+        Ok(_) => (),
+        Err(_) => return Err(Status::InternalServerError),
     };
 
     match recipes::table
         .order(recipes::id.desc())
-        .first::<Recipes>(connection) {
-            Ok(recipe) => Ok(Json(recipe)),
-            Err(_) => Err(Status::InternalServerError),
+        .first::<Recipe>(connection)
+    {
+        Ok(recipe) => Ok(Json(recipe)),
+        Err(_) => Err(Status::InternalServerError),
     }
 }
 
--- backend/src/models.rs
+++ backend/src/models.rs
@@ -1,23 +1,56 @@
+use crate::schema::{instructions, recipes};
+use chrono;
 use diesel::prelude::*;
 use rocket::serde::{Deserialize, Serialize};
-use crate::schema::recipes;
-use chrono;
 
 #[derive(Serialize, Queryable)]
 #[serde(crate = "rocket::serde")]
-pub struct Recipes {
+pub struct Recipe {
     pub id: i32,
     pub title: String,
     pub servings: String,
     pub created_at: Option<chrono::NaiveDateTime>,
-    pub updated_at: Option<chrono::NaiveDateTime>
+    pub updated_at: Option<chrono::NaiveDateTime>,
 }
-
 
 #[derive(Insertable, Deserialize)]
 #[serde(crate = "rocket::serde")]
 #[diesel(table_name = recipes)]
 pub struct RecipesInput {
     pub title: String,
-    pub servings: String
+    pub servings: String,
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq, Serialize)]
+#[serde(crate = "rocket::serde")]
+#[diesel(belongs_to(Recipe))]
+pub struct Instruction {
+    pub id: i32,
+    pub instruction: String,
+    pub display_order: i32,
+    pub recipe_id: i32,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct RecipeResultDTO {
+    pub id: i32,
+    pub title: String,
+    pub servings: String,
+    pub instructions: Vec<String>,
+    pub created_at: Option<chrono::NaiveDateTime>,
+    pub updated_at: Option<chrono::NaiveDateTime>,
+}
+
+impl From<Recipe> for RecipeResultDTO {
+    fn from(r: Recipe) -> Self {
+        Self {
+            id: r.id,
+            title: r.title,
+            servings: r.servings,
+            instructions: Vec::<String>::new(),
+            created_at: r.created_at,
+            updated_at: r.updated_at,
+        }
+    }
 }
